d1ba013186115dc0b21f03c1aab56d250175f1a4
Peter Rugg <pdr32@cam.ac.uk>
2024-06-04 15:06:55 +0100

    [testrig] Use in-core tag controller for testrig
    The TestRIG toplevel has its own tags. Just wire these to zero, as
    the core can now keep track of tags internally.

--- dv/testrig/ibex_top_sram.sv
+++ dv/testrig/ibex_top_sram.sv
@@ -64,9 +64,9 @@ module ibex_top_sram import ibex_pkg::*; #(
   output logic                         data_we_o,
   output logic [3:0]                   data_be_o,
   output logic [31:0]                  data_addr_o,
-  output logic [32:0]                  data_wdata_o,
+  output logic [31:0]                  data_wdata_o,
   output logic [6:0]                   data_wdata_intg_o,
-  input  logic [32:0]                  data_rdata_i,
+  input  logic [31:0]                  data_rdata_i,
   input  logic [6:0]                   data_rdata_intg_i,
   input  logic                         data_err_i,
 
